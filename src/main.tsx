40ecc47abdc7fba42d6e4ad4fb998cb9ed36b34f
Zuraiz <Zuraiz.zafar97@gmail.com>
2026-04-24 23:25:25 -0400

    use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./App.tsx";
 import { ThemeProvider } from "./providers/ThemeProvider.tsx";
 import { UserProvider } from "./providers/UserProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <UserProvider>
           <App />
         </UserProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rcf-bbq-2026/',
+  base: '/',
   plugins: [tailwindcss(), react()],
 })
