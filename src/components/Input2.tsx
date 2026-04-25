e40d1831961b59f549dca6e46412d934884265a7
Zuraiz <Zuraiz.zafar97@gmail.com>
2026-04-24 23:38:17 -0400

    ignore case

--- src/components/Input2.tsx
+++ src/components/Input2.tsx
@@ -3,6 +3,13 @@ import userFavoriteGamesLoader from "../assets/user_favorite_games_loader.json";
 import type { Theme as ThemeType } from "../providers/ThemeProvider";
 import { useUser } from "../providers/UserProvider";
 
+const userFavoriteGames = Object.entries(userFavoriteGamesLoader).reduce(
+  (acc, [user, game]) => {
+    acc[user.toLowerCase()] = game.favorite_game as ThemeType;
+    return acc;
+  },
+  {} as Record<string, string>,
+);
 export const Input = () => {
   const { setTheme } = useTheme();
   const { discord_username, setDiscordUsername } = useUser();
@@ -10,11 +17,11 @@ export const Input = () => {
     setDiscordUsername(e.target.value);
 
     const user =
-      userFavoriteGamesLoader[
-        e.target.value.toLowerCase() as keyof typeof userFavoriteGamesLoader
+      userFavoriteGames[
+        e.target.value.toLowerCase() as keyof typeof userFavoriteGames
       ];
     if (user) {
-      setTheme(user.favorite_game as ThemeType);
+      setTheme(user as ThemeType);
     }
   };
   return (
